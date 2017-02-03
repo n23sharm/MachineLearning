f954300ff878f52b0a6f44fa242ec72b8f19a892
Neha Sharma <nehasharma@Viveks-MacBook-Air.local>
2017-02-03 00:22:30 -0500

    regularized cost and gradient

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -18,7 +18,22 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 
 
+h = sigmoid(X*theta);
 
+a = (y .* -1)' * log(h);
+b = (1 - y)' * log(1 - h);
+
+unregularizedCost = (a - b) / m;
+theta(1) = 0;
+
+regularizedCost = ((lambda/(2*m)) * (theta'*theta));
+
+J = unregularizedCost + regularizedCost;
+
+unregularizedGrad = (X' * (h - y))/m;
+regularizedGrad = (lambda/m) * theta;
+
+grad = unregularizedGrad + regularizedGrad;
 
 
 
